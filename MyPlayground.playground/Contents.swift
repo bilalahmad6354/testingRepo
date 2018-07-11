0243dde94392781b24940a874c0d6b274bb03b9f
Bilal Ahmad <bilalahmad@MAC03.local>
2018-07-11 11:51:08 +0500

    This is the first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+var myString:String?
+myString = "Hello, Swift 4!"
+
+if let yourString = myString {
+    print("Your string has - \(yourString)")
+} else {
+    print("Your string does not have a value")
+}
